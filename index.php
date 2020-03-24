2ec226d4783c2ebaaaf51a66deff60323e3c617d
phongsakornchantapoon <59908421+phongsakornchantapoon@users.noreply.github.com>
2020-03-24 11:04:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <H1>
 <?php echo "รหัสนิสิต-62310134"; ?><br>
 <?php echo "ชื่อ-สกุล นาย พงศกร จันทาพูน"; ?><br>
-<iframe width="500" height="500"
-src="https://www.youtube.com/watch?v=oTn9FAofAnk">
-</iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/oTn9FAofAnk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </BODY>
 </HTML>
