c62e4a65bbc8ec25377fc6e24876dd9130ed1e0f
phongsakornchantapoon <59908421+phongsakornchantapoon@users.noreply.github.com>
2020-03-24 10:36:47 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310134"; ?><br>
+<?php echo "ชื่อ-สกุล นาย พงศกร จันทาพูน"; ?><br>
+<iframe width="500" height="500"
+src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
+</iframe>
+</BODY>
+</HTML>
